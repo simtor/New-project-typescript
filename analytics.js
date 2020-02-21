53f0b15db57fd631180bd0a9fd183a34e305c03d
simtor <simone.ram@ada.ac.uk>
2020-02-21 11:58:06 +0000

    Created button and learned about sourcemap in the  tsconfig

--- analytics.js
+++ analytics.js
@@ -2,3 +2,4 @@
 console.log('Sending...');
 // const sum =(num1: number, num2: number):number => num1 + num2;
 // export default sum;
+//# sourceMappingURL=analytics.js.map
--- app.js
+++ app.js
@@ -8,3 +8,4 @@ const button = document.querySelector('button');
 button.addEventListener('Click', () => {
     console.log('I was clicked');
 });
+//# sourceMappingURL=app.js.map
